68c321162e0f03f084ba9ae40512070183b5ea61
zb666 <1360799362@qq.com>
2019-04-08 16:49:00 +0800

    新增Readme，重新思索基础概念

--- lib/1-container.dart
+++ lib/1-container.dart
@@ -1,10 +1,17 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 void main()=>runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Builder(builder: (BuildContext context){
+        return FlatButton(onPressed: (){
+          Scaffold.of(context).showSnackBar(SnackBar(content: Text('点击弹出SnackBar弹窗')));
+        }, child: Text('Button'));
+      }),
+    );
   }
 }
